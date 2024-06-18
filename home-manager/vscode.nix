a95ff837b010cf6ad024b77982ebfac3a6ef32c3
MegaDrage <dragonpsui@gmail.com>
2024-06-18 09:20:25 +0700

    VSCode additions

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -1,31 +1,5 @@
 {pkgs, ...}: {
     programs.vscode = {
         enable = true;
-        package = pkgs.vscodium;  
-        userSettings = {
-            "files.autoSave" = "afterDelay";
-            "editor.fontFamily" = "'JetBrains Mono'";
-            "editor.fontSize" = 15;
-            "editor.fontLigatures" = true;
-            "editor.letterSpacing" = 0.4;
-            "editor.smoothScrolling" = true;
-            "workbench.iconTheme" = "vscode-icons";
-        };
-        extensions = with pkgs.vscode-extensions; [
-            yzhang.markdown-all-in-one
-            jnoortheen.nix-ide
-            mkhl.direnv
-            arrterian.nix-env-selector
-            llvm-vs-code-extensions.vscode-clangd 
-            ms-python.python
-            #kiteco.kite
-            #EricSia.pythonsnippets3
-            aaron-bond.better-comments
-            bungcip.better-toml
-            # SonarSource.sonarlint-vscode
-            vadimcn.vscode-lldb
-            # TeamHub.teamhub
-            vscode-icons-team.vscode-icons
-        ];
     };
 }
